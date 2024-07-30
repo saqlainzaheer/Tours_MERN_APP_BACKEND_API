e76f79d7dab86d9a2f89e88500df661972bc2e13
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-07-30 18:46:25 +0500

    response issue resolve

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -6,7 +6,6 @@ export const createEmployees = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      status: 200,
 
       message: 'Employees Added Successfully',
       data: {
