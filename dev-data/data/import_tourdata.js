d8bd515f90aa8f8246ed81b2354a01169ce783c4
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-08-03 22:30:42 +0500

    importing data

---
+++ dev-data/data/import_tourdata.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+import { Tour } from '../../models/Tour.js';
+
+dotenv.config();
+const __dirname = path.resolve();
+const DB = process.env.DATABASE;
+const dbUser = process.env.NODE_ENV;
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch((err) => console.log('MongoDB connection failed:', err));
+
+console.log(`Connecting to database at ${DB} with user ${dbUser}`);
+const tours = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, 'dev-data/data/tours-simple.json'),
+    'utf-8'
+  )
+);
+
+const import_tour = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  import_tour();
+}
