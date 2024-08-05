98b81c4c7bcc3a81798023144fcf223a761d0d76
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-08-05 13:41:09 +0500

    some cahnge

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { createEmployees } from '../controllers/employeeController.js';
 
 const router = express.Router();
-
+console.log('Sd');
 router.route('/multiple').post(createEmployees); //store multiple employees
 
 export default router;
